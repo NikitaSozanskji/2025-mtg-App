340f94ab9988f4f0449249ee109fd0e725e4f2a2
Nikita Sozanskyi <N00241754@iadt.ie>
2025-10-21 16:01:53 +0100

    edit2

--- resources/views/components/card-form.blade.php
+++ resources/views/components/card-form.blade.php
@@ -95,4 +95,42 @@
             {{ isset($card) ? 'Update Card' : 'Add Card' }}
         </x-primary-button>
     </div>
+
+    
+</form>
+
+@props(['action', 'method', 'card'])
+
+<form action="{{ $action }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    @if($method === 'PUT' || $method === 'PATCH')
+        @method($method)
+    @endif
+
+    <div class="mb-4">
+        <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
+        <input 
+            type="text" 
+            name="title" 
+            id="title" 
+            value="{{ old('title', $card->title ?? '') }}" 
+            required 
+            class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+        />
+        @error('title')
+            <p class="text-sm text-red-600">{{ $message }}</p>
+        @enderror
+    </div>
+
+    @isset($card->image)
+        <div class="mb-4">
+            <img src="{{ asset('images/cards/' . $card->image) }}" alt="{{ $card->title }}" class="w-24 h-32 object-cover">
+        </div>
+    @endisset
+
+    <div>
+        <x-primary-button>
+            {{ isset($card) ? 'Update Book' : 'Add Book' }}
+        </x-primary-button>
+    </div>
 </form>
