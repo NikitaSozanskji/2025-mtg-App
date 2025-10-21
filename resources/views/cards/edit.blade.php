fac5807ae86afae5c5f1e3468e962e6438658601
Nikita Sozanskyi <N00241754@iadt.ie>
2025-10-21 15:56:04 +0100

    edit part 1

--- resources/views/cards/edit.blade.php
+++ resources/views/cards/edit.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create New card') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="font-semibold text-lg mb-4">Edit New card:</h3>
+
+                    {{-- Using the cardForm component for card creation --}}
+                    <x-card-form
+                        :action="route('cards.update', $card)"
+                        :method="'PUT'"
+                        :card="$card"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -16,12 +16,26 @@
                     <h3 class="font-semibold text-lg mb-4">List of Cards</h3>
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                         @foreach($cards as $card)
-                        <a href="{{route('cards.show',$card)}}">
-                            <x-card-card
-                                :card_name="$card->card_name"
-                                :image="$card->image"
-                            />
-                        </a>
+                        <div class="">
+                            <a href="{{route('cards.show',$card)}}">
+                                <x-card-card
+                                    :card_name="$card->card_name"
+                                    :image="$card->image" />
+                            </a>
+                            <!-- Edit and Delete Buttons -->
+                            <div class="mt-4 flex space-x-2">
+                                <a href="{{ route('cards.edit', $card) }}" class="text-gray-600 bg-orange-300 hover:bg-orange-700 font-bold py-2 px-4 rounded">
+                                    Edit
+                                </a>
+                                <form action="{{ route('cards.destroy', $card) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this card?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-gray-600 font-bold py-2 px-4 rounded">
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
                         @endforeach
                     </div>
                 </div>
