0ff9ea9db7bfa0d69d7f77f1492ff9bad04db4aa
Nikita Sozanskyi <N00241754@iadt.ie>
2025-11-24 14:42:51 +0000

    ok

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -61,6 +61,31 @@
                     <textarea name="comment" id="comment" rows="3" class="mt-1 block w-full" placeholder="Write your coment here..."></textarea>
                 </div>
 
+                @foreach($card->reviews as $review)
+                    <li class="bg-gray-100 p-4 rounded-lg">
+                        <p class="font-semibold">{{ $review->user->name }} ({{ $review->created_at->format('M d, Y') }})</p>
+                        <p>Rating: {{ $review->rating }} / 5</p>
+                        <p>{{ $review->coment }}</p>
+                        @if ($review->user->is(auth()->user()) || auth()->user()->role === 'admin')
+
+                            <a href="{{ route('reviews.edit', $review) }}" 
+                            class="bg-yellow-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+                                {{ __('Edit Review') }}
+                            </a>
+
+                            <form method="POST" action="{{ route('reviews.destroy', $review) }}">
+                                @csrf
+                                @method('delete')
+                                <x-danger-button 
+                                    href="{{ route('reviews.destroy', $review) }}"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    {{ __('Delete Review') }}
+                                </x-danger-button>
+                            </form>
+                        @endif
+                    </li>
+                @endforeach
+
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Submit Review
                 </button>
