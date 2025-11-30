490ed0d859342aae64cb8b5a7607dba2477612e0
Nikita Sozanskyi <N00241754@iadt.ie>
2025-11-30 23:38:25 +0000

    many to many

---
+++ database/migrations/2025_11_30_183842_create_artists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image')->nullable(); // For profile headshot
+            $table->text('bio')->nullable();     // Paragraph about the author
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artists');
+    }
+};
